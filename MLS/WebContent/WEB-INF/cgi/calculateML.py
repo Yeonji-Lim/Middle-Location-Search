f62154684ea0c5234d2c1080714f65634fdcd1e1
Yeonji-Lim <view7186@naver.com>
2021-11-28 21:04:48 +0900

    python 통신 성공

--- MLS/WebContent/WEB-INF/cgi/calculateML.py
+++ MLS/WebContent/WEB-INF/cgi/calculateML.py
@@ -1,68 +1,53 @@
-#! /Library/Frameworks/Python.framework/Versions/3.8/bin/python3
+#!/usr/local/bin/python3
 
-import numpy as np
-import matplotlib.pyplot as plt
+# import cgi  # python을 cgi로 사용
+# import request  # 결과 post 
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 print("content-type:text/html; charset=UTF-8\n")
 
-# def make_circle(c, r):
-#     theta = np.linspace(0, 2 * np.pi, 256)
-#     x = r * np.cos(theta)
-#     y = r * np.sin(theta)    
-#     return np.vstack((x, y)).T + c
+# 원적합 확인을 위한 원 만드는 함수 
+"""
+def make_circle(c, r):
+    theta = np.linspace(0, 2 * np.pi, 256)
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)    
+    return np.vstack((x, y)).T + c
+"""
 
-# c = np.array([2, 3])
-# r = 1.5
-
-# circle = make_circle(c, r)
-
-# # circle 변수에서 20개 추출
-# idx = np.random.choice(len(circle), 15)
-# A = circle[idx]
-
-# # 가우시안 노이즈 추가
-# A = A + 0.09 * np.random.randn(A.shape[0], A.shape[1])
+# data from client
+# form = cgi.FieldStorage()
+# latlng = form["latlng"].value
+# print("test : {}".format(latlng))
 
 # 임의의 좌표 추가, ndarray 형식 변환
-data = [[1,1], [3, -1], [0, 4], [5, 6]]
-A = np.array(data)
+# data = [[1,1], [3, -1], [0, 4], [5, 6]]
+# A = np.array(data)
 
-ones = np.ones((A.shape[0], 1))
-A = np.concatenate((ones, A), axis = 1)
-b = A[:,1] ** 2 + A[:,2] ** 2
+# ones = np.ones((A.shape[0], 1))
+# A = np.concatenate((ones, A), axis = 1)
+# b = A[:,1] ** 2 + A[:,2] ** 2
 
-c = np.linalg.inv(A.T.dot(A)).dot(A.T).dot(b)
-# or you can do this just by using 
-# np.linalg.lstsq(A, b, rcond=None)[0]
+# c = np.linalg.inv(A.T.dot(A)).dot(A.T).dot(b)
+# # or you can do this just by using 
+# # np.linalg.lstsq(A, b, rcond=None)[0]
 
-def return_circle(c):
-    x_c = c[1] / 2
-    y_c = c[2] / 2
-    r = c[0] + x_c ** 2 + y_c ** 2
+# def return_circle(c):
+#     x_c = c[1] / 2
+#     y_c = c[2] / 2
+#     r = c[0] + x_c ** 2 + y_c ** 2
     
-    return x_c, y_c, np.sqrt(r)
+#     return x_c, y_c, np.sqrt(r)
 
-x_c, y_c, r_c = return_circle(c)
-print(x_c, y_c, r_c)
+# x_c, y_c, r_c = return_circle(c)
+# print(x_c, y_c, r_c)
 
+# 원적합 확인 
+"""
 fitted_circle = make_circle(np.array([x_c, y_c]), r_c)
 
-# plt.figure(figsize = (12,4))
-
 plt.figure(figsize = (4,4))
-# plt.plot(circle[:,0], circle[:, 1], 'b-')
-# plt.scatter(x = A[:,0], y = A[:, 1], color = 'r')
-# plt.grid()
-# plt.xlim(0,5)
-# plt.ylim(0,5)
-# plt.show()
-
-# plt.subplot(131)
-# plt.plot(circle[:,0], circle[:, 1], 'b-', label = 'original_circle')
-# plt.scatter(x = A[:,1], y = A[:, 2], color = 'r', label = 'sampled point')
-# plt.grid()
-# plt.xlim(0,5)
-# plt.ylim(0,5)
 
 plt.subplot(132)
 plt.plot(fitted_circle[:,0], fitted_circle[:, 1], 'y-', label = 'fitted_circle')
@@ -71,13 +56,24 @@ plt.grid()
 plt.xlim(0,20)
 plt.ylim(0,20)
 
-# plt.subplot(133)
-# plt.plot(circle[:,0], circle[:, 1], 'b-', label = 'original_circle')
-# plt.plot(fitted_circle[:,0], fitted_circle[:, 1], 'y-', label = 'fitted_circle')
-# plt.grid()
-# plt.legend()
-# plt.xlim(0,5)
-# plt.ylim(0,5)
-
-# plt.tight_layout()
 plt.show()
+"""
+
+print('''
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form name="hiden_form" action="./calculateML" method="POST">
+        <input type="hidden" name="latlng" value="1234"/>
+        <input type="submit" />
+    </form>
+	<script>
+		document.hiden_form.submit();
+	</script>
+</body>
+</html>
+''')
