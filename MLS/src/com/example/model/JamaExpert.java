d4b7a78456f0d40a01bb9208da5e8cf40e8e1866
Yeonji-Lim <view7186@naver.com>
2021-12-03 15:25:32 +0900

    연산 후 통신 완료

--- MLS/src/com/example/model/JamaExpert.java
+++ MLS/src/com/example/model/JamaExpert.java
@@ -3,39 +3,34 @@ package com.example.model;
 import Jama.Matrix;
 
 public class JamaExpert {
-	public double getResult(double[][] x) {
+	public double[] calculateML(double[][] x) {
 						
-		Matrix A = new Matrix(4, 3);
-		double ad[][] = A.getArray();
+		int loc_num = x.length;
+		Matrix A = new Matrix(loc_num, 3);
+		double[][] ad = A.getArray();
 		
-		for(int i = 0; i < 4; i++) {
+		for(int i = 0; i < loc_num; i++) {
 			ad[i][0] = 1;
 			ad[i][1] = x[i][0];
 			ad[i][2] = x[i][1];
 		}
-//		ad[0][1] = 1;
-//		ad[0][2] = 1;
-//		ad[1][1] = 3;
-//		ad[1][2] = -1;
-//		ad[2][1] = 0;
-//		ad[2][2] = 4;
-//		ad[3][1] = 5;
-//		ad[3][2] = 6;
 		
-		Matrix b = new Matrix(4, 1);
-		double ad2[][] = b.getArray();
-		for(int i = 0; i < 4; i++) {
+		Matrix b = new Matrix(loc_num, 1);
+		double[][] ad2 = b.getArray();
+		for(int i = 0; i < loc_num; i++) {
 			ad2[i][0] = Math.pow(ad[i][1], 2)+Math.pow(ad[i][2], 2); 
 		}
 				
 		//c = (A^T*A)^(-1)*A^T*b -> 사이즈는 3, 1
 		Matrix c = A.transpose().times(A).inverse().times(A.transpose()).times(b);
 		
-		double res[][] = c.getArray();
-		double x_c = res[1][0]/2;
-		double y_c = res[2][0]/2;
-		double r_c = Math.sqrt(res[0][0] + Math.pow(x_c, 2) + Math.pow(y_c, 2));
+		double[][] ad3 = c.getArray();
+		double x_c = ad3[1][0]/2;
+		double y_c = ad3[2][0]/2;
+		double r_c = Math.sqrt(ad3[0][0] + Math.pow(x_c, 2) + Math.pow(y_c, 2));
 		
-		return(r_c);
+		double[] result = {x_c, y_c, r_c};
+		
+		return(result);
 	}
 }
--- MLS/src/com/example/web/calcMLServlet.java
+++ MLS/src/com/example/web/calcMLServlet.java
@@ -2,7 +2,6 @@ package com.example.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,46 +16,25 @@ import org.json.simple.parser.ParseException;
 
 import com.example.model.JamaExpert;
 
-
-/**
- * Servlet implementation class calcMLServlet
- */
 @WebServlet("/calcMLServlet")
 public class calcMLServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public calcMLServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.setContentType("text/html");
+		response.setContentType("application/x-json; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
-		//String json_str = request.getParameter("locations");
-		String json_str = "[{ \"lat\": \"1\", \"lng\":\"1\" }, { \"lat\": \"3\", \"lng\":\"-1\" }, { \"lat\": \"0\", \"lng\":\"4\" }, { \"lat\": \"5\", \"lng\":\"6\" },]";
 		JSONParser parser = new JSONParser();
 		JSONArray arr = null;
 		JSONObject obj = null;
 		 
 		try {
-		     arr = (JSONArray)parser.parse(json_str);
+		     arr = (JSONArray)parser.parse(request.getParameter("locations"));
 		} catch (ParseException e) {
 		     System.out.println("변환에 실패");
 		     e.printStackTrace();
@@ -66,15 +44,13 @@ public class calcMLServlet extends HttpServlet {
 		
 		for(int i = 0; i < arr.size(); i++) {
 			obj = (JSONObject)arr.get(i);
-			data[i][0] = Double.parseDouble((String)obj.get("lat"));
-			data[i][1] = Double.parseDouble((String)obj.get("lng"));
+			data[i][0] = (double)obj.get("lat");
+			data[i][1] = (double)obj.get("lng");
 		}
 		
-		double x = Double.parseDouble(request.getParameter("latlng"));
 		JamaExpert j = new JamaExpert();
-		
-		double result = j.getResult(data);
-		out.println(result);
+		double[] result = j.calculateML(data);
+		out.println("{ \"x\" : "+result[0]+", \"y\" : "+result[1]+"}");
 	}
 
 }
